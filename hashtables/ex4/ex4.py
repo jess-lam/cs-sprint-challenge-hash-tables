71f3a58c85ff41016de249a638813b82fb4fb02a
Jessica Lam <jemlam93@gmail.com>
2020-11-08 20:44:54 -0800

    completed ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,22 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+
+    for num in a:
+        if num != 0:
+            if num in cache:
+                cache[num] += 1
+            else:
+                cache[num] = 1
+                reverse = num * (-1)
+                if reverse in cache:
+                    if reverse > num:
+                        result.append(reverse)
+                    else:
+                        result.append(num)
+    return result
 
 
 if __name__ == "__main__":
